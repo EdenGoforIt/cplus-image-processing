843a5ed346dcf2d4f839ce365bcd1222c38c8a4b
Eden Park <edengoforit@gmail.com>
2025-03-12 19:34:36 +1300

    ch3 affine

--- ch3/src/affine.cpp
+++ ch3/src/affine.cpp
@@ -5,10 +5,25 @@
 
 using namespace cv;
 using namespace std;
- 
 
-int main( )
+int main()
 {
+	Mat image = imread("../akiyo1.jpg");
+	Mat image2;
+	image2.create(image.size(), image.channels());
+
+	Point2f center;
+	center.x = image.cols / 2;
+	center.y = image.rows / 2;
+
+	double angle = 30;
+	double scale = 1;
+
+	Mat rotation = getRotationMatrix2D(center, angle, scale);
+	warpAffine(image, image2, rotation, image.size());
+
+	imshow("source", image);
+	imshow("output", image2);
 
 	waitKey(0);
 	return 0;
