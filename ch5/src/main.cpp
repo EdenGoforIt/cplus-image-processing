a630ca04576282a96b09575d3cd5986513f99225
Eden Park <edengoforit@gmail.com>
2025-04-09 20:14:23 +1200

    feat: hsv to bgr

--- ch5/src/main.cpp
+++ ch5/src/main.cpp
@@ -8,6 +8,55 @@ using namespace std;
 
 int main()
 {
+	Mat image = imread("../akiyo1.jpg");
+
+	// HSV to RGB
+	// Mat hsv;
+	// cvtColor(image, hsv, COLOR_BGR2HSV);
+
+	// vector<Mat> channels(3);
+	// split(hsv, channels);
+	// imshow("Hue", channels[0]);
+	// imshow("Saturation", channels[1]);
+	// imshow("Value", channels[2]);
+
+	// Mat classified = Mat::zeros(image.size(), image.type());
+	// for (int y = 0; y < hsv.rows; y++)
+	// {
+	// 	for (int x = 0; x < hsv.cols; x++)
+	// 	{
+	// 		Vec3b pixel = hsv.at<Vec3b>(y, x);
+	// 		int h = pixel[0];
+	// 		int s = pixel[1];
+
+	// 		Vec3b &out = classified.at<Vec3b>(y, x);
+	// 		if (s < 15)
+	// 		{
+	// 			out = Vec3b(0, 0, 0); // black
+	// 		}
+	// 		else if (h < 10 || h > 160)
+	// 		{
+	// 			out = Vec3b(0, 0, 255); // Red
+	// 		}
+	// 		else if (h >= 10 && h < 25)
+	// 		{ // Orange
+	// 			out = Vec3b(0, 128, 255);
+	// 		}
+	// 		else if (h >= 25 && h < 45)
+	// 		{ // Yellow
+	// 			out = Vec3b(0, 255, 255);
+	// 		}
+	// 		else if (h >= 45 && h < 65)
+	// 		{ // Green
+	// 			out = Vec3b(0, 255, 0);
+	// 		}
+	// 	}
+	// }
+
+	// imshow("HSV Classification", classified);
+
+	// RGB to HSV
+
 	// RGB
 	// It's about showing the intensity of rgb into the image; if blue colors are intense, it shows as white
 	// Mat image = imread("../akiyo1.jpg");
