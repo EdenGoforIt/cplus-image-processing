8351e58196c89ca67d7bc4f78bcfd91ade2c830e
Eden Park <edengoforit@gmail.com>
2025-04-09 19:41:45 +1200

    feat: rgb split channels

--- ch5/src/main.cpp
+++ ch5/src/main.cpp
@@ -8,14 +8,17 @@ using namespace std;
 
 int main()
 {
+	// RGB
+	// It's about showing the intensity of rgb into the image; if blue colors are intense, it shows as white
+	// Mat image = imread("../akiyo1.jpg");
+	// vector<Mat> channels(3);
+	// split(image, channels);
 
-	Mat image = imread("../akiyo1.jpg");
-	if (image.empty())
-	{
-		cerr << "Error: Could not open image file." << endl;
-		return -1;
-	}
+	// imshow("original", image);
+	// imshow("Blue", channels[0]);
+	// imshow("Green", channels[1]);
+	// imshow("Red", channels[2]);
 
-		waitKey(0);
+	waitKey(0);
 	return 0;
 }
